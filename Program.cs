9756692b8b3e69f04158b55bc88530af78c0d6af
dealer426 <burnssamuel01@gmail.com>
2020-01-23 09:23:59 -0500

    Added an Array and sorted the array

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace sprint3sortnumbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //Create an Array
+            int[] numbers = new int[10] {10,4,5,7,3,5,8,2,1,9};
+
+            //user the Array api and sort your numbers
+            Array.Sort(numbers);
+
+            //loop through the numbers and print them to the screen in order
+            foreach(int val in numbers)
+            {
+                Console.WriteLine(val + " ");
+            }
+
+            Console.ReadKey();
         }
     }
 }
